7423ed9eea2deabee6a24fc12adfa82b65156791
Dudosyka <a.hatson@ya.ru>
2020-02-06 21:42:49 +0300

    Exception view update.
    Clearing buffer before displaying.

--- core/View.php
+++ core/View.php
@@ -19,31 +19,40 @@ use app\widgets\HTML5Layout;
 
 class View
 {
+    public function fatal_exception_render($data = [])
+    {
+        ob_clean();
+        ob_start();
+        extract($data);
+        include_once "Exception/ExceptionView/view_error.php";
+        ob_flush();
+    }
+
     public function render($view_name, $data = [], HTMLTag $head = NULL)
     {
+        ob_start();
         $view_dirname = AppConfig::$default_view_dir;
         $view_filename = AppConfig::$default_view;
         $view_name = explode("/", trim($view_name, "/"));
         if (isset($view_name[1]))
         {
-            $view_dirname = $view_name[0];
+            $view_dirname = $view_name[0]."/";
             $view_filename = $view_name[1];
         }
         else if (isset($view_name[0]))
         {
             $view_filename = $view_name[0];
         }
-        $view_name = "$view_dirname/$view_filename.php";
+        $view_name = "$view_dirname$view_filename.php";
         $template_path = '../views/templates/'.AppConfig::$layout;
         if (!file_exists('../views/'.$view_name))
         {
-            $data['view_error'] = "Вида с именем $view_name не существует";
-            $template_path = "../views/templates/template_error.php";
-            $view_name = "view_error.php";
+            throw new \ViewException("error while loading view. View with name $view_name wasn`t found.");
         }
         extract($data);
         $head = (is_null($head)) ? new HTMLTag("head") : $head;
         $layout = new HTML5Layout($head);
         include $template_path;
+        ob_flush();
     }
 }
